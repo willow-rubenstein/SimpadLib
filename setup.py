7a57a49bf455f7fd9701236787c921e72b624592
malevtuber <contact.notashe@gmail.com>
2022-01-06 12:12:15 -0500

    moved from setup.cfg to setup.py because long readme would not show up on pypi

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from pathlib import Path
+
+setup(
+    name='simpadlib',
+    author='Ashe Muller',
+    author_email='contact.notashe@gmail.com',
+    version='1.0.2',
+    description='A library that allows users to custom-configure their SimPad real-time',
+    long_description=(Path(__file__).parent / "PIP_README.md").read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/malevtuber/simpadlib',
+    package_dir={'': 'src'},
+    python_requires='>=3.10',
+    install_requires=['pywinusb>=0.4.2'],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/malevtuber/simpadlib/issues'
+    }
+)
