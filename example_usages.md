97b561f13d4058751204b1b2429dc522d1f894cc
malevtuber <contact.notashe@gmail.com>
2022-01-08 14:43:33 -0500

    i made a small error oops

--- example_usages.md
+++ example_usages.md
@@ -1,18 +1,18 @@
-# SimpadLib v2.1
+# SimpadLib v2.2
 
 ### Example Usages
 ```py
-from simpad.lib import simpad
+from simpadlib import simpad
 
 sim = simpad.Controller() # Initialize the SimPad class
-sim.changeRGB("#FF000", keys=simpad.Keys.Both) # Change all keys to color "#FF0000"
-sim.changeRGB("#00FF00", keys=simpad.keys.Left) # Change the left key's color to "#00FF00"
+sim.changeRGB("#FF000", keys=simpad.Keys.both) # Change all keys to color "#FF0000"
+sim.changeRGB("#00FF00", keys=simpad.keys.left) # Change the left key's color to "#00FF00"
 ```
 
 ```py
-from simpad.lib import simpad
+from simpadlib import simpad
 
 sim = simpad.Controller() # Initialize the SimPad class
-sim.changeRGB("#FF000", keys=simpad.Keys.Both, brightness="25%") # Change all keys to color "#FF0000", but only with 25% brightness
-sim.changeRGB("#00FF00", keys=simpad.keys.Left, brightness="50%") # Change the left key's color to "#00FF00", but only with 50% brightness
+sim.changeRGB("#FF000", keys=simpad.Keys.both, brightness="25%") # Change all keys to color "#FF0000", but only with 25% brightness
+sim.changeRGB("#00FF00", keys=simpad.keys.left, brightness="50%") # Change the left key's color to "#00FF00", but only with 50% brightness
 ```
--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from pathlib import Path
 ## I'm just going to manually do this for now.
 ## I don't think there is an easy way to fix the bug with readme files.
 description = """\
-# SimpadLib v2.1
+# SimpadLib v2.2
 
 ### Example Usages
 ```py
-from simpad.lib import simpad
+from simpadlib import simpad
 
 sim = simpad.Controller() # Initialize the SimPad class
-sim.changeRGB("#FF000", keys=simpad.Keys.Both) # Change all keys to color "#FF0000"
-sim.changeRGB("#00FF00", keys=simpad.keys.Left) # Change the left key's color to "#00FF00"
+sim.changeRGB("#FF000", keys=simpad.Keys.both) # Change all keys to color "#FF0000"
+sim.changeRGB("#00FF00", keys=simpad.keys.left) # Change the left key's color to "#00FF00"
 ```
 
 ```py
-from simpad.lib import simpad
+from simpadlib import simpad
 
 sim = simpad.Controller() # Initialize the SimPad class
-sim.changeRGB("#FF000", keys=simpad.Keys.Both, brightness="25%") # Change all keys to color "#FF0000", but only with 25% brightness
-sim.changeRGB("#00FF00", keys=simpad.keys.Left, brightness="50%") # Change the left key's color to "#00FF00", but only with 50% brightness
+sim.changeRGB("#FF000", keys=simpad.Keys.both, brightness="25%") # Change all keys to color "#FF0000", but only with 25% brightness
+sim.changeRGB("#00FF00", keys=simpad.keys.left, brightness="50%") # Change the left key's color to "#00FF00", but only with 50% brightness
 ```
 """
 
@@ -29,7 +29,7 @@ setup(
     name='simpadlib',
     author='Ashe Muller',
     author_email='contact.notashe@gmail.com',
-    version='2.0.1',
+    version='2.0.2',
     description='A library that allows users to interface with their SimPad devices in real-time',
     long_description=description,
     long_description_content_type='text/markdown',
